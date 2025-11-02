3f27278d2c5995434ff22f3078377bd4c6bfd4d6
Jason Z <jason26214@gmail.com>
2025-11-02 23:41:57 +1030

    create movie schema

---
+++ src/models/movie.model.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+// Review schema (will be embedded in Movie schema)
+const reviewSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Movie schema
+const movieSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    types: {
+      type: [String],
+      required: true,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [reviewSchema], // Embedding reviews
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model('Movie', movieSchema);
