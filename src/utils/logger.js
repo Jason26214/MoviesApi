5d3d23af3e242d69a6bd124be83c522135d5ed62
Jason Z <jason26214@gmail.com>
2025-11-03 16:47:16 +1030

    reconfig logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,20 +4,17 @@ const config = require('./config');
 
 const createLogger = (filename) => {
   const logger = winston.createLogger({
-    // read log level from .env
     level: config.LOG_LEVEL || 'info',
     defaultMeta: {
       file: filename ? path.basename(filename) : undefined,
     },
+
     format: winston.format.combine(
       winston.format.timestamp(),
-      winston.format.printf(({ timestamp, level, message, file }) => {
-        // add file's name in logger
-        return `[${timestamp}]${
-          file ? ` [${file}]` : ''
-        } [${level}] [${message}]`;
-      })
+      winston.format.errors({ stack: true }), // <--- (A) find error from stack
+      winston.format.json() // <--- (B) default format is JSON
     ),
+
     transports: [
       new winston.transports.File({
         filename: 'logs/error.log',
@@ -26,9 +23,31 @@ const createLogger = (filename) => {
       new winston.transports.File({
         filename: 'logs/combined.log',
       }),
-      new winston.transports.Console(),
     ],
   });
+
+  // If not in production, log to the console with a colorful format
+  if (config.NODE_ENV !== 'production') {
+    logger.add(
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss',
+          }),
+          winston.format.printf(
+            ({ level, message, stack, file, timestamp }) => {
+              const fileLabel = file ? ` [${file}]` : '';
+              return `${timestamp} ${level}${fileLabel}: ${stack || message}`;
+            }
+          )
+        ),
+      })
+    );
+  } else {
+    // In production, log to console in JSON format. Easy to read by log management tools (like Datadog, Kibana).
+    logger.add(new winston.transports.Console());
+  }
 
   return logger;
 };
